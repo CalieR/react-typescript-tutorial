6841897a6dc763f22c037e6a6cd8cce6b0281dd6
CalieR <calielrushton@gmail.com>
2024-05-17 10:18:38 +0100

    First exercise - how to find clues and answers

--- src/01-introduction/03-navigating-jsx-types.problem.tsx
+++ src/01-introduction/03-navigating-jsx-types.problem.tsx
@@ -2,11 +2,12 @@ export const Component = () => {
   return (
     <div
       // How do I figure out what type aria-posinset expects?
-      aria-posinset={}
+      aria-posinset={123}  // expects a number or undefined
       // How do I figure out what type onChange expects?
-      onChange={}
+      onChange={() => {}} // expects some sort of handler
 
       // How do I get autocomplete with JSX?
+      // ctrl + space bar to bring up a list of options
     />
   );
 };
